a1aa66cfc749101d5cd1a5628ba1fd62c5d59da9
SK207_19 <SK207_19@K207_19.carrollcc.edu>
2016-12-05 20:52:57 -0500

    Finished product.

--- src/main/java/edu/carrollcc/cis232/Question1.java
+++ src/main/java/edu/carrollcc/cis232/Question1.java
@@ -3,25 +3,18 @@ package edu.carrollcc.cis232;
 public class Question1 {
 
 	public static void main(String[] args) {
-		
-	}
-	
-	public void run() throws IllegalArgumentException {
 
 	}
 
 	public static int maxElement(int[] array) {
+		if (array == null || array.length == 0) {
+			throw new IllegalArgumentException("Empty Array");
+		}
 		return maxElement(array, 0);
 	}
 
 	private static int maxElement(int[] array, int index) {
-		try {
-			return index < array.length ? Math.max(array[index], maxElement(array, index + 1)) : Integer.MIN_VALUE;
-			
-		} catch (IllegalArgumentException e) {
-			System.out.println(e.getMessage());
-		}
-		
 		return index < array.length ? Math.max(array[index], maxElement(array, index + 1)) : Integer.MIN_VALUE;
+
 	}
 }
--- src/main/java/edu/carrollcc/cis232/Question2.java
+++ src/main/java/edu/carrollcc/cis232/Question2.java
@@ -5,21 +5,22 @@ import java.util.Scanner;
 public class Question2 {
 
 	public static void main(String[] args) {
-		/*Scanner keyboard = new Scanner(System.in);
+		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Enter Palindrome:");
 		String pal = keyboard.nextLine();
 		if (isPalindrome(pal))
 			System.out.println(pal + " is a palindrome");
 		else
-			System.out.println(pal + " is not a palindrome");*/
+			System.out.println(pal + " is not a palindrome");
 	}
 
 	public static boolean isPalindrome(String string) {
-		if (string.replaceAll("[^0-9]","").toLowerCase().length() == 0 || string.replaceAll("[^0-9]","").toLowerCase().length() == 1)
+		String clean = string.toUpperCase().replaceAll("[^A-Z0-9]","");
+		if (clean.length() == 0 || clean.length() == 1)
 			return true;
-		if (string.toLowerCase().charAt(0) == string.toLowerCase().charAt(string.length() - 1))
+		if (clean.charAt(0) == clean.charAt(clean.length() - 1))
 		
-			return isPalindrome(string.substring(1, string.length() - 1));
+			return isPalindrome(clean.substring(1, clean.length() - 1));
 
 		return false;
 	}
