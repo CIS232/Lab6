113a26b026f4dfba31c126240ed1b478a1a974d5
Zach <zachdugan@rocketmail.com>
2016-12-07 15:37:40 -0500

    Update Question1.java

--- src/main/java/edu/carrollcc/cis232/Question1.java
+++ src/main/java/edu/carrollcc/cis232/Question1.java
@@ -2,9 +2,20 @@ package edu.carrollcc.cis232;
 
 public class Question1 {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IllegalArgumentException{
+		int[] x = new int[]{4,8,15,16,23,42};
+		System.out.println(MaxElement(x,(x.length-1)));
 	}
 
 	//Write static maxElement method here
+	public static int MaxElement(int[] array, int counter) throws IllegalArgumentException
+	{
+	    if (counter > 0) 
+	    {
+	        return Math.max(array[counter], MaxElement(array, counter-1));
+	    } else 
+	    {
+	        return array[0];
+	    }
+	}
 }
