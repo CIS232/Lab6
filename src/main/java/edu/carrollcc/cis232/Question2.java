51e71404f0c41bca6a35ac0ec0344eec36805925
Zach <zachdugan@rocketmail.com>
2016-12-07 16:45:37 -0500

    Update Question2.java

--- src/main/java/edu/carrollcc/cis232/Question2.java
+++ src/main/java/edu/carrollcc/cis232/Question2.java
@@ -3,8 +3,26 @@ package edu.carrollcc.cis232;
 public class Question2 {
 
 	public static void main(String[] args) {
+		String initPhrase1 = "I saw desserts; I'd no lemons, alas no melon. Distressed was I.";
+		String initPhrase2 = "Now saw ye no mosses or foam, or aroma of roses! So money was won.";
+		String initPhrase3= "Ere hypocrisies or poses are in, my hymn I erase. So prose I, sir, copy here.";
 
+		String finalPhrase=(initPhrase1.replaceAll("[-+.'|{};?! ^:,]","")).toLowerCase();
+		
+        	if(isPalindrome(finalPhrase)){
+            		System.out.println(initPhrase1 + " is a palindrome!");
+		}else{
+            		System.out.println("That's not a palindrome.");
+		}
 	}
 	
-	//write static isPalindrome method here
+	public static boolean isPalindrome(String phrase){
+		if(phrase.length() < 2){
+			return true; 
+		}
+		if(phrase.charAt(0) == phrase.charAt(phrase.length()-1)){
+			return isPalindrome(phrase.substring(1, phrase.length()-1));
+		}
+		return false;
+    	}
 }
