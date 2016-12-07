573453d8300f23f51f0f80ba2b0be7679c8f25ee
Justin Doan <JustinDoan@mail.com>
2016-12-07 18:12:54 -0500

    My solution to Chapter 15 lab

--- src/main/java/edu/carrollcc/cis232/Question2.java
+++ src/main/java/edu/carrollcc/cis232/Question2.java
@@ -5,6 +5,21 @@ public class Question2 {
 	public static void main(String[] args) {
 
 	}
-	
-	//write static isPalindrome method here
+
+	public static boolean isPalindrome(String string) {
+
+		String cleanedString = string.toUpperCase().replaceAll("[^A-Z0-9]", "");
+
+		if (cleanedString.length() <= 1) {
+			return true;
+		}
+
+		if (cleanedString.charAt(0) == cleanedString.charAt(cleanedString.length() - 1)) {
+
+			String leftoverString = cleanedString.substring(1, cleanedString.length() - 1);
+			
+			return isPalindrome(leftoverString);
+		}
+		return false;
+	}
 }
